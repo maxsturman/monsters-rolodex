9833a0df3a45a73aee9a0539d1e971b25ff572e4
Max Sturman <36490238+maxsturman@users.noreply.github.com>
2020-04-21 13:16:36 +0100

    29 passed mapping

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import './card-list.styles.css';
 
 export const CardList = props => ( 
-  <div className='card-list'>{props.children}</div>
+  <div className='card-list'>
+    {props.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+  </div>
 );
