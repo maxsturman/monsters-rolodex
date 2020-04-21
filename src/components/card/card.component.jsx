ed90f3decf51c5b900b8b9d1fdb1650a25e101de
Max Sturman <36490238+maxsturman@users.noreply.github.com>
2020-04-21 13:27:37 +0100

    Lesson 29 / unique images imported using ID's

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = props => (
-  <div>
+  <div className='card-container'>
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
     <h1> {props.monster.name} </h1>
   </div>
 );
