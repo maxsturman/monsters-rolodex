bf49438268905f52d31075114144f2474e529b6a
Max Sturman <36490238+maxsturman@users.noreply.github.com>
2020-04-21 14:25:30 +0100

    Added Search Box to record first keypress

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,6 +6,6 @@ export const Card = props => (
     <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
     />
     <h2> {props.monster.name} </h2>
-    <p> {props.monster.email } </p>
+    <p> {props.monster.email} </p>
   </div>
 );
