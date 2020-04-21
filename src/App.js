69e3061ddc1e95172e861614e0b5d16f4f776015
Max Sturman <36490238+maxsturman@users.noreply.github.com>
2020-04-21 14:38:48 +0100

    Added Filter function on CardList

--- src/App.js
+++ src/App.js
@@ -21,12 +21,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className='App'>
-        <input type='search' placeholder='Search Monsters' onChange={e =>  
+        <input 
+        type='search' 
+        placeholder='Search Monsters' 
+        onChange={e =>  
           this.setState({ searchField: e.target.value})}
       />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
